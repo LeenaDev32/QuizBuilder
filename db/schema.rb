bc29d8475fdeeb00fca75d195099cfe85b1d0ae1
Leena Bhise <bleenaweb238@gmail.com>
2022-06-05 13:44:12 +0530

    Added user roles and name field

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_04_142205) do
+ActiveRecord::Schema.define(version: 2022_06_05_080733) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 2022_06_04_142205) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "role", default: 0
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(name: 'Joe Bloggs', role: :admin, email: 'joe@yopmail.com', password: 'password')
+User.create(name: 'Jane Doe', role: :user, email: 'jane@yopmail.com', password: 'password')
